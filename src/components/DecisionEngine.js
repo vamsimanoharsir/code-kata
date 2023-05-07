4e2f701a14a516f45361a3f1e308e0b3cca3c459
Vamsi Manohar Komma <Manohar.Komma@ivycomptech.com>
2023-05-07 18:21:08 +0530

    commit of the entire project

---
+++ src/components/DecisionEngine.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import './styles.css'
+
+function DecisionEngine(props) {
+    const name=useParams()
+    
+    return (
+        <div>
+            <h2 className='headingend'>Final details that are sent to the decision engine after applyimg rules to summarize application</h2><br/><br/><br/><br/><br/><br/><br/>
+            <h3>Final result including preAssessment values as specified : </h3><br/>{JSON.stringify(name)}<br/><br/>
+
+
+            <Link className='Link5' to='/'>Home Page</Link>
+
+        </div>
+    );
+}
+
+export default DecisionEngine;
---
+++ src/components/LoanForm.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { useState } from 'react';
+import styled from 'styled-components';
+import CheckForm from './CheckForm';
+import { Link } from 'react-router-dom';
+
+function LoanForm(props) {
+    
+    const [name, setName] = useState('');
+    const [date, setDate] = useState(0);
+    const [loan,setLoan]=useState(0)
+    const [option, setOption] = useState('');
+    const [submittedData, setSubmittedData] = useState(null);
+  
+    const CenterDiv = styled.div`
+        position: fixed;
+        top: 30%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    `
+    
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      setSubmittedData({ name, date, loan, option });
+    };
+  
+    return (
+        <div className='form'>
+            <center>
+            <form onSubmit={(e) => { handleSubmit(e) }}>
+                <br /><p className='title'>Please fill the details</p><br />
+                <div className='container'>
+                <label>
+                    Name:
+                    <input className='input' type="text" value={name} onChange={(e) => setName(e.target.value)} required/>
+                </label>
+                <br /><br />
+                <label>
+                    Year:
+                    <input className='input' type="number" value={date} onChange={(e) => setDate(e.target.value)} required/>
+                </label>
+                <br /><br />
+                <label>
+                    Loan amount:
+                    <input className='input' type="number" value={loan} onChange={(e) => setLoan(e.target.value)} required/>
+                </label>
+                <br /><br />
+                <label>
+                    Accounting provider:<br/>
+                    <select className='input' value={option} onChange={(e) => setOption(e.target.value)}>
+                        <option value="">Select an option</option>
+                        <option value="option1">Xero</option>
+                        <option value="option2">MYOB </option>
+                        <option value="option3" disabled>we only have two providers</option>
+                    </select>
+                </label>
+                <br /><br />
+                <button className='input' type="submit">click here to save details</button><br /><br />
+                </div>
+            </form>
+            <br/><Link className='Link3' to={`/checkform/${name}/${date}/${loan}`}>Request Balance Sheet</Link>
+            </center>
+        </div>
+    )
+}
+
+export default LoanForm;
